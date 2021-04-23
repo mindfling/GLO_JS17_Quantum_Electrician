73d620223490a0eef3762b1a853d9ee9d33e2687
DimMindfling <dimendroider@gmail.com>
2021-04-23 19:38:05 +0300

    feat make sliderCarousel with options

--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@
 import togglePopup from './modules/togglePopup';
 import sendForm from './modules/sendForm';
 
+import Carousel from './modules/sliderCarousel';
 
 togglePopup();
 sendForm();
 
+// ЗАПУСК СЛАЙДЕРА КАРУСЕЛИ
+// const opty = {
+//   main: '.services-elements',
+//   wrap: '.services-carousel',
+//   next: '.arrow-right',
+//   prev: '.arrow-left',
+//   slidesToShow: 3,
+//   infinity: true
+// };
+
+// const carousel = new Carousel(opty);
+// carousel.init();
+
+new Carousel({
+  main: '.services-elements',
+  wrap: '.services-carousel',
+  next: '.arrow-right',
+  prev: '.arrow-left',
+  slidesToShow: 3,
+  infinity: true,
+}).init();
