5684f10eb516b690932824fd6f74466534d7a1b8
DimMindfling <dimendroider@gmail.com>
2021-04-24 16:33:22 +0300

    feat make it smooth scrollintoview

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@
 
 import togglePopup from './modules/togglePopup';
 import sendForm from './modules/sendForm';
-
+import handleMenu from './modules/handleMenu.js';
 import Carousel from './modules/sliderCarousel';
 
 togglePopup();
+handleMenu();
 sendForm();
 
 // ЗАПУСК СЛАЙДЕРА КАРУСЕЛИ
@@ -18,15 +19,23 @@ sendForm();
 //   slidesToShow: 3,
 //   infinity: true
 // };
-
 // const carousel = new Carousel(opty);
-// carousel.init();
 
-new Carousel({
-  main: '.services-elements',
-  wrap: '.services-carousel',
-  next: '.arrow-right',
-  prev: '.arrow-left',
-  slidesToShow: 3,
-  infinity: true,
-}).init();
+const carousel = new Carousel({
+    main: '.services-elements',
+    wrap: '.services-carousel',
+    next: '.arrow-right',
+    prev: '.arrow-left',
+    slidesToShow: 3,
+    infinity: true,
+});
+carousel.init();
+
+// new Carousel({
+//   main: '.services-elements',
+//   wrap: '.services-carousel',
+//   next: '.arrow-right',
+//   prev: '.arrow-left',
+//   slidesToShow: 3,
+//   infinity: true,
+// }).init();
