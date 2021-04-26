1c99e81ebcac6c9c64622bb2a16e971cc5c47504
DimMindfling <dimendroider@gmail.com>
2021-04-26 11:17:21 +0300

    feat scrollUp disappear exactly at top servicesSection block

--- src/modules/scrollUp.js
+++ src/modules/scrollUp.js
@@ -1,40 +1,39 @@
 const scrollUp = () => {
   const servicesSection = document.querySelector('#services');
-  const faqSection = document.querySelector('#faq');
-  const contactsSection = document.querySelector('#contacts');
+  // const faqSection = document.querySelector('#faq');
+  // const contactsSection = document.querySelector('#contacts');
   const mainWrapper = document.querySelector('.main-wrapper');
-
   const headerWrapper = document.querySelector('.header-wrapper'); //блок хедера
-  // console.log('headerWrapper: ', headerWrapper);
-
+  const blockHeaderHight = headerWrapper.getBoundingClientRect().height;
   const upButton = document.querySelector('.up');
+  // скрываем кнопку вверх в самом начале :)
   upButton.classList.add('animate__animated');
-  upButton.classList.toggle('animate__bounceInUp');
+  upButton.classList.add('animate__bounceOutDown');
 
   upButton.addEventListener('click', (event) => {
     event.preventDefault();
-    mainWrapper.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
+    // плавная прокрутка до самого верхнего блока mainWrapper
+    mainWrapper.scrollIntoView({behavior: 'smooth', block: 'start'});
   });
 
-  //событие прокрутки scroll
+  // отслеживаем событие прокрутки scroll до нужного блока чтобы отбразить шеврон up на нужном уровне + 1 еще для точности
   document.addEventListener('scroll', (event) => {
-    if (document.documentElement.scrollTop > 1000) {
-      // todo добавить точные координаты блока
+
+    if (servicesSection.getBoundingClientRect().top < blockHeaderHight + 1) {
+      // проскрорили ниже блока НАШИ УСЛУГИ
 
       // * showUpButton ВЫПРЫГИВАЕТ С НИЗУ ВВЕРХ
-      upButton.classList.add('animate__animated');
+      // upButton.classList.add('animate__animated');
       upButton.classList.remove('animate__bounceOutDown');
-      upButton.classList.add('animate__bounceInUp');
+      upButton.classList.add('animate__bounceInUp'); // замена display block
     } else {
       // * showUpButton СКРЫВАЕМ ВНИЗ
-      upButton.classList.add('animate__animated');
-      upButton.classList.add('animate__bounceOutDown');
+      // upButton.classList.add('animate__animated');
+      upButton.classList.add('animate__bounceOutDown'); // замена display none
       upButton.classList.remove('animate__bounceInUp');
     }
   });
 };
+
 
 export default scrollUp;
