3efc66f5068072aad0ce481c8ea0f5a262b84e86
DimMindfling <dimendroider@gmail.com>
2021-04-26 11:05:54 +0300

    feat try to make deligation on handleMenu

--- src/modules/handleMenu.js
+++ src/modules/handleMenu.js
@@ -1,52 +1,61 @@
 const handleMenu = () => {
-  console.log('lets handle menu');
+  const topMenu = document.querySelector('.top-menu');
 
-  const topMenu = document.querySelector('.top-menu'); // ! все верхнее меню
-
-  // todo размер хэдера сверху нужно учитывать
+  // todo высота хэдера сверху нужно учитывать
   const headerWrapperHeight = document.querySelector('.header-wrapper').getBoundingClientRect().height;
 
-  // пункты меню ссылки на соответствующие блоки
+  // todo просто а потом делегирование пункты меню ссылки на соответствующие блоки
   const targetServices = topMenu.querySelector('a[href="#services"]');
   const targetFaq = topMenu.querySelector('a[href="#faq"]');
   const targetContacts = topMenu.querySelector('a[href="#contacts"]');
-
-  // todo use it
-  const targets = topMenu.querySelectorAll('a');
-  console.log('targets: ', targets);
 
   // todo найдем блоки куда нужно крутить
   const servicesSection = document.querySelector('#services');
   const faqSection = document.querySelector('#faq');
   const contactsSection = document.querySelector('#contacts');
 
-  // todo клики здесь можно делигировать
-  // пункт services
+  // todo ДЕЛЕГИРУЕМ клики
+  const targets = topMenu.querySelectorAll('a');
+  topMenu.addEventListener('click', (event) => {
+    event.preventDefault();
+    let target = event.target;
+    console.log('target: ', target);
+
+    targets.forEach((item) => {
+      if (item === target) {
+        const id = event.target.getAttribute('href'); // находим значение в ссылке
+        document.querySelector(id).scrollIntoView({
+          behavior: 'smooth', // плавная прокрутка
+          block: 'start', // к началу блока
+        });
+      }
+    });
+  });
+
+};
+
+export default handleMenu;
+
+
+
+
+  /*
   targetServices.addEventListener('click', (event) => {
     event.preventDefault(); // отключаем действие клика по ссылке по умолчанию
-    servicesSection.scrollIntoView({
-      behavior: 'smooth', // плавная прокрутка
-      block: 'start', // к началу блока
-    });
+    // плавная прокрутка // к началу блока
+    servicesSection.scrollIntoView({ behavior: 'smooth',  block: 'start',  });
   });
 
   targetFaq.addEventListener('click', (event) => {
     event.preventDefault();
-    const id = event.target.getAttribute('href'); //todo можно использовать attr href
+    const id = event.target.getAttribute('href')
     console.log(id);
-    faqSection.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start', // блок прокручивается чтобы его scrollTop был = 0
-    });
+     // блок прокручивается чтобы его scrollTop был = 0 
+    faqSection.scrollIntoView({ behavior: 'smooth', block: 'start', });
   });
 
   targetContacts.addEventListener('click', (event) => {
     event.preventDefault();
-    contactsSection.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
+    contactsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
   });
-};
-
-export default handleMenu;
+*/
