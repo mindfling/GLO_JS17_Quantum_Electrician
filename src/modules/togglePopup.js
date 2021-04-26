2bfe046d0f570e1614317f5d6fcfd63ad115be67
DimMindfling <dimendroider@gmail.com>
2021-04-26 18:39:32 +0300

    feat fix popup animation

--- src/modules/togglePopup.js
+++ src/modules/togglePopup.js
@@ -1,4 +1,3 @@
-// ! Модуль обработки модальных окон
 /**
  * По клику на кнопку “Заказать звонок” !! .callback-btn 2 кнопки
  * открывается модальное окно с классом // * .modal-callback
@@ -7,7 +6,8 @@
 /**
  * В модальном окне при клике на крестик с классом // * .modal-close
  * или на подложку // * .modal-overlay
- * модальное окно и подложка должны скрываться.
+ * модальное окно и подложка должны скрываться
+ * * модальное окно появляется и скрывается через анимацию animate.css 2020 !!
  */
 
 const togglePopup = () => {
@@ -17,22 +17,28 @@ const togglePopup = () => {
   const modalOverlay = document.querySelector('.modal-overlay'); // подложка под окно
   const modal = document.querySelector('.modal-callback'); // модальное окно
 
+  const servicesSection = document.querySelector('#services'); // клики по блокам карусели Наши Услуги
+  const servicesElements = servicesSection.querySelectorAll('.element');
+
   // отобразить модальное окно
   const showModal = () => {
     modalOverlay.style.display = 'block';
     modal.style.display = 'block';
-    // todo анимация через animate.css
-    modal.classList.remove('fadeOut');
-    modal.classList.add('animated', 'fadeIn');
+    // анимация через animate.css 2020
+    modal.classList.remove('animate__fadeOut');
+    modal.classList.add('animate__animated', 'animate__fadeIn');
   };
 
   // скрыть модальное окно
   const closeModal = () => {
-    modal.style.display = 'none';
-    modalOverlay.style.display = 'none';
-    // todo сделать анимацию animate.css
-    modal.classList.remove('fadeIn');
-    modal.classList.add('animated', 'fadeOut');
+    // сделать анимацию animate.css
+    modal.classList.add('animate__animated', 'animate__fadeOut');
+    modal.classList.remove('animate__fadeIn');
+    // можно сделать это через promise по совету animate.css
+    setTimeout(() => {
+      modal.style.display = 'none';
+      modalOverlay.style.display = 'none';
+    }, 500);
   };
 
   // * клпик по кнопкам Заказать звонок callback-btn Открывает окно
@@ -42,6 +48,13 @@ const togglePopup = () => {
 
   // * клик по кнопке ЗВОНИТЕ, МЫ ОБЯЗАТЕЛЬНО ВАМ ПОМОЖЕМ!
   buttonServices.addEventListener('click', (event) => showModal());
+
+  //? * клик по элементу карусели из блока Наши услуги services-section тоже открывает окно
+  servicesSection.querySelectorAll('.element').forEach((item) => {
+    item.addEventListener('click', (event) => {
+      showModal();
+    });
+  });
 
   // * клик по overlay Закрывает окно
   modalOverlay.addEventListener('click', (event) => {
@@ -53,6 +66,5 @@ const togglePopup = () => {
     closeModal();
   });
 };
-
 
 export default togglePopup;
